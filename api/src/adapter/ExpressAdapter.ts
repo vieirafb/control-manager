d57c0fc3fa3354cd7328d5386c90978854c8aee8
Luiz F. Vieira <luizfernando8966@gmail.com>
2024-06-28 09:52:34 -0300

    Bug: Corrige recebimento de parâmetro via URL

--- api/src/adapter/ExpressAdapter.ts
+++ api/src/adapter/ExpressAdapter.ts
@@ -2,7 +2,7 @@ import {NextFunction, Request, Response} from 'express';
 
 export default function ExpressAdapter(fn: Function) {
     return async function (req: Request, res: Response, next: NextFunction) {
-        const obj = await fn(req.params, req.body);
+        const obj = await fn(req.params, Object.assign({}, req.query, req.body));
         if (obj.error) return next(obj.error);
         return res.status(obj.status).json(obj.response);
     }
